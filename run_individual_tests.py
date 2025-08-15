b6d9cf795a0d8370e96895c038a3ce809809547b
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-08-15 14:38:17 +0900

    result.txtがすでにある場合は削除

--- run_individual_tests.py
+++ run_individual_tests.py
@@ -1,6 +1,11 @@
 import subprocess
+import os
 
 def run_tests_and_append_results(start=1, end=10, output_file="result.txt"):
+    # すでにファイルが存在していたら削除
+    if os.path.exists(output_file):
+        os.remove(output_file)
+        
     with open(output_file, "a", encoding="utf-8") as outfile:
         for i in range(start, end + 1):
             test_file = f"tests/test_q{i:02d}.py"
